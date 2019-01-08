892d0345f7a8728485a7d2ff64c7742312a8ebe5
archer123 <liuziyi345@gmail.com>
2019-01-08 10:21:48 -0800

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -36,10 +36,12 @@ secend = 0
 
 listseq= []
 for f in s:
+	print "new file"
 	cptid += 1
 	fp = open(f)
 	pcap = dpkt.pcap.Reader(fp)
 	for ts, buf in pcap:
+        	#print ts, len(buf)
 		if gettimebegin:
 			tsbegin = ts
 			gettimebegin = False
@@ -47,18 +49,26 @@ for f in s:
 		eth = dpkt.ethernet.Ethernet(buf)
 		ip = eth.data
 		tcp = ip.data
+
 		if len(tcp.data) > 0 and not (str(tcp.seq) in listseq):
 			listseq.append(str(tcp.seq))
 		#print (socket.inet_ntoa(ip.dst) == server)
 			if socket.inet_ntoa(ip.dst) == server:
 				#print ts, len(tcp.data)
 				cptcli += len(tcp.data)
-                if cptsec == 0:
-                    secbegin = ts
-                    secend = ts
+                if ((tcp.flags & dpkt.tcp.TH_SYN) and (tcp.flags & dpkt.tcp.TH_ACK)):
+        		ackinit = tcp.ack
+        		ack = ackinit
+        		#print "1", ack, ackinit, tcp.ack
+    		if ((tcp.flags & dpkt.tcp.TH_ACK ) and not (tcp.flags & dpkt.tcp.TH_PUSH)) :
+        		if ack < tcp.ack:
+            			ack = tcp.ack
+            			#print "2", ack, ackinit, tcp.ack
+        #fout.write(secbegin+"\t"+ secend+"\t"+ str(8*cptsec/(secend-secbegin))+"\n")
+        #fout.close()
 
 
-	strcli = str(cptid) +  "\t" + str(tsbegin) + "\t" + str(tsend) + "\t" + str( 8* cptcli/((tsend - tsbegin)*1000000)) + "\n\n"
+	strcli = str(cptid) +  "\t" + str(tsbegin) + "\t" + str(tsend) + "\t" + str( 8* (ack - ackinit)/((tsend - tsbegin)*1000000)) + "\n\n"
 	out.write(strcli)
 
 	cptcli = 0
